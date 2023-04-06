f75b5d587839e183cf9931d4a2a99f13cf444a42
Shefat Tonmoy <shefathossain7@gmail.com>
2023-04-07 00:22:03 +0600

    done

---
+++ src/components/Orders/Orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            <h2>Order Review Page</h2>
+        </div>
+    );
+};
+
+export default Orders;
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Layout from './components/Layout/Layout';
+import Orders from './components/Orders/Orders';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () => fetch('t-shirts.json')
+      },
+      {
+        path: '/orders',
+        element: <Orders></Orders>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
